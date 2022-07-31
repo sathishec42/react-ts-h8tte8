ffbcbbf5024b4ed45a6ab794800ea7f29cfc4c2d
sathishec42 <sathishec42@gmail.com>
2022-07-31 23:01:35 +0530

    2 files modified

--- App.tsx
+++ App.tsx
@@ -1,7 +1,25 @@
 import * as React from 'react';
 import './style.css';
+const axios = require('axios').default;
+
 
 export default function App() {
+
+
+
+// Make a request for a user with a given ID
+axios.get('https://docs.google.com/spreadsheets/d/e/2PACX-1vR1mSyGYTXL_au7O06YmO-Bbg3jmJOSAEW_l95w6WSr9rTbItdEqcZQShcmKQoS6QevvZdJRn-cniWx/pubhtml')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
